c85235b3807fbc22fb388497d4cde82f6ac379f8
James Brown <james@catch22.net>
2023-06-13 11:13:15 +0100

    Update __init__.py

--- src/andersen_ev/__init__.py
+++ src/andersen_ev/__init__.py
@@ -6,7 +6,7 @@ Python library for controlling the Andersen A2 EV car charger.
 # For relative imports to work in Python 3.6
 import os, sys; sys.path.append(os.path.dirname(os.path.realpath(__file__)))
 
-__version__ = "{{VERSION_PLACEHOLDER}}"  #0.1.0
+__version__ = "0.1.0"
 __author__  = 'James Brown'
 __credits__ = 'Catch22'
 
